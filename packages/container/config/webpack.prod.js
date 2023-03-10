49ee4a6c445641a8e7ea682ce2ae1574f2b442e7
is3388 <meiychin@yahoo.com>
2023-03-10 15:32:41 -0500

    Added publicPath to the prod config and added AWS default region to the script

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -8,8 +8,10 @@ const domain = process.env.PRODUCTION_DOMAIN // defined through CI/CD pipeline
 const prodConfig = {
   mode: 'production', // in PROD mode, webpack makes sure all JS files build get minified and optimized
   output: {
-    filename:'[name].[contenthash].js' // name of the file created and hash the file content for caching issue
+    filename:'[name].[contenthash].js',
+     // name of the file created and hash the file content for caching issue
     // filename template
+    publicPath: '/container/latest' //prepend this path to all files that added script tags by htmlWebpackplugin
   },
   plugins: [ // remoteEntry files point to PROD domain not localhost:port #
     new ModuleFederationPlugin({
