8a2e564bda94fcd387df6a19e5be71fe58163ae7
is3388 <meiychin@yahoo.com>
2023-03-13 14:58:34 -0400

    Added header component and fixed css classname conflict

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,13 +1,24 @@
 import React from 'react'
 import MarketingApp from './components/MarketingApp'
+import { BrowserRouter } from 'react-router-dom'
+import Header from './components/Header'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co' // generate all classname with prefix of ma and only affects marketing project
+})
 
 const App = () => {
   return (
+    <BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
     <div>
-      <h1>Hi there!!!</h1>
-      <hr />
+      <Header />
       <MarketingApp />
     </div>
+    </StylesProvider>
+    </BrowserRouter>
+    
   )
 }
 
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,13 +1,17 @@
-import { StylesProvider } from '@material-ui/core/styles'
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles'
 import React from 'react'
 import { Switch, Route, BrowserRouter } from 'react-router-dom'
 import Landing from './components/Landing'
 import Pricing from './components/Pricing'
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma' // generate all classname with prefix of ma and only affects marketing project
+})
+
 const App = () => {
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             <Route path='/pricing' component={Pricing} exact />
